869e00fe9daa5416330698d5c24dfafa9b430389
quxionglie <qu.xionglie@payeco.com>
2018-07-16 00:39:43 +0800

    q001 readme

--- src/main/java/com/quxionglie/leecode/q001/Solution.java
+++ src/main/java/com/quxionglie/leecode/q001/Solution.java
@@ -4,6 +4,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
+ * https://leetcode-cn.com/problems/two-sum/description/
+ * 给定一个整数数组和一个目标值，找出数组中和为目标值的两个数。
+ * 你可以假设每个输入只对应一种答案，且同样的元素不能被重复利用。
+ *
+ * 复杂度分析：
+ *  时间复杂度：O(n)
+ *  空间复杂度：O(n)
  * Created on 2018/7/15.
  *
  * @author qu.xionglie
